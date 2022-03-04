a8941daf3520dedfe9b2ca1cab263e921cef2a2d
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-03-04 20:31:34 +0100

    input start parametr works

---
+++ FirstUnityProject/Assets/Scripts/InputBehaviour.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputBehaviour : MonoBehaviour
+{
+    private int minVal = 1;
+    private int maxVal = 10;
+    private InputField input;
+    private Button nextButton;
+    public int N { get; set; }
+    
+    void Awake()
+    {
+        input = GameObject.Find("InputField").GetComponent<InputField>();
+        nextButton = GameObject.Find("NextButton").GetComponent<Button>();
+        N = -1;
+    }
+
+    public void OnInputPut(string inputString) {
+        int inputInt = -1;
+        try
+        {
+            inputInt = System.Int32.Parse(inputString);
+        }
+        catch (System.FormatException e)
+        {
+            GameObject.Find("ErrorText").GetComponent<Text>().text = $"Wrong input format, must be number.";
+            return;
+        }
+        if(inputInt < minVal || inputInt > maxVal) {
+            GameObject.Find("ErrorText").GetComponent<Text>().text = $"{inputInt} is not in the range <{minVal}, {maxVal}>";
+            return;
+        }
+        N = inputInt;
+        GameObject.Find("ErrorText").GetComponent<Text>().text = $"";
+    }
+    public void OnMouseDown()
+    {
+        if(N == -1) {
+            GameObject.Find("ErrorText").GetComponent<Text>().text = $"Value not set.";
+            return;
+        }
+    }
+}
