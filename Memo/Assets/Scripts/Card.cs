7e728f798e92844a7bf9074a3d3a02cc27950abb
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-03-11 14:00:19 +0100

    images added, lost/win text works

--- Memo/Assets/Scripts/Card.cs
+++ Memo/Assets/Scripts/Card.cs
@@ -1,41 +1,49 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
-    // public static int allDisplayed = 0;
     public static List<Card> DisplayedCards = new List<Card>();
     public int MatchingId {get; set;}
-    public bool IsDisplayed {get; set;}
+    public bool WasGuessed { get; set; }
+
+    private static Sprite back;
+
+    void Awake()
+    {
+        back = Resources.Load("Cards/back", typeof(Sprite)) as Sprite;
+    }
+
     void Start()
     {
         gameObject.GetComponentInChildren<Text>().enabled = false;
+        WasGuessed = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void SetText(string text) {
-        gameObject.GetComponentInChildren<Text>().text = text;
+    private Sprite getFront() {
+        return Resources.Load($"Cards/{MatchingId}", typeof(Sprite)) as Sprite;
     }
 
     public void OnClick() {
         if(DisplayedCards.Count < 2 && !DisplayedCards.Contains(this)) {
-            gameObject.GetComponentInChildren<Text>().enabled = true;
+            gameObject.GetComponent<Button>().image.sprite = getFront();
             DisplayedCards.Add(this);
         }
     }
 
     public void Hide() {
-        gameObject.GetComponentInChildren<Text>().enabled = false;
+        gameObject.GetComponent<Button>().image.sprite = back;
     }
 
     public void Remove() {
         gameObject.SetActive(false);
+        WasGuessed = true;
+    }
+
+    public void Reset() {
+        gameObject.GetComponent<Button>().image.sprite = back;
+        gameObject.SetActive(true);
+        WasGuessed = false;
     }
 }
--- Memo/Assets/Scripts/GameController.cs
+++ Memo/Assets/Scripts/GameController.cs
@@ -8,95 +8,120 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public static int SizeId { get; set; }
-    private int score = 0;
-    private int rowsNr = 2;
-    private int colsNr = 2;
+    private int score = 0, rowsNr = 2, colsNr = 2;
     private Card[] cards;
 
     private Card gameObjectPattern;
-    private Vector2 vectorUp;
-    private Vector2 vectorRight;
-    private Vector2 vectorAngleUp;
-    private Vector2 vectorAngleDown;
-    private Vector2[] setupVectors;
     private bool TempBlock = false;
+    private Text scoreText, gameEndText;
 
     private static System.Random random = new System.Random();
+
     void Awake()
     {
-        Card.DisplayedCards = new List<Card>();
-        if(SizeId >= 1) {
-            colsNr = 4;
-        }
-        if(SizeId >= 2) {
-            rowsNr = 4;
-        }
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        gameEndText = GameObject.Find("GameEndText").GetComponent<Text>();
+        if(SizeId >= 1) { colsNr = 4; }
+        if(SizeId >= 2) { rowsNr = 4; }
+        SetupCards();
+        setNewGame();
+    }
+
+    private void SetupCards() {
         cards = new Card[rowsNr * colsNr];
         gameObjectPattern = Resources.Load("Card", typeof(Card)) as Card;
-        var cameraY = Camera.main.orthographicSize * 2f;
-        var cameraX = cameraY * Camera.main.aspect;
-        
-        Canvas cameraMiddle = GameObject.Find("GameCanvas").GetComponent<Canvas>();
-        vectorUp = new Vector2(0, 120);
-        vectorRight = new Vector2(85, 0);
-        vectorAngleUp = (vectorRight + vectorUp) / 2;
-        vectorAngleDown = (vectorRight - vectorUp) / 2;
+        // var cameraY = Camera.main.orthographicSize * 2f;
+        // var cameraX = cameraY * Camera.main.aspect;
+        var canvas = GameObject.Find("GameCanvas");
+        Canvas cameraMiddle = canvas.GetComponent<Canvas>();
+        Vector2 vectorUp = new Vector2(0, 120 + 5);
+        Vector2 vectorRight = new Vector2(85 + 5, 0);
+        Vector2 vectorAngleUp = (vectorRight + vectorUp) / 2;
+        Vector2 vectorAngleDown = (vectorRight - vectorUp) / 2;
 
-        setupVectors = new Vector2[]{
+        Vector2[] setupVectors = new Vector2[]{
             vectorAngleUp, vectorAngleDown, -vectorAngleUp, -vectorAngleDown,
             vectorAngleUp + vectorRight, vectorAngleDown + vectorRight, -vectorAngleUp - vectorRight, -vectorAngleDown - vectorRight,
             vectorAngleUp*3, -vectorAngleUp*3, vectorAngleDown*3, -vectorAngleDown*3,
             vectorAngleUp + vectorUp, -vectorAngleUp - vectorUp, vectorAngleDown - vectorUp, -vectorAngleDown + vectorUp,
         };
+
         for(int i = 0; i < rowsNr * colsNr; i++) {
             cards[i] = Instantiate(gameObjectPattern) as Card;
-            cards[i].transform.SetParent (GameObject.Find("GameCanvas").transform, false);
+            cards[i].transform.SetParent (canvas.transform, false);
             cards[i].transform.position = setupVectors[i] + (Vector2) cameraMiddle.transform.position;
         }
+    }
+
+    private void setNewGame() {
+        Card.DisplayedCards = new List<Card>();
         cards = cards.OrderBy(x => random.Next()).ToArray();
         for(int i = 0; i < cards.Count(); i++) {
             cards[i].MatchingId = i % (cards.Count() / 2);
-            cards[i].SetText($"{i % (cards.Count() / 2)}");
+            cards[i].Reset();
         }
     }
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        // Debug.Log(Card.allDisplayed);
+        if(score <= -10) {
+            EndLost();
+        }
+        else if(cards.Where(x => !x.WasGuessed).Count() == 0)
+        {
+            EndWon();
+        }
+        else {
+            runGame();
+        }
+    }
+
+    private void EndLost() {
+        foreach (var item in cards)
+            {
+                item.gameObject.SetActive(false);
+            }
+            scoreText.text = $"Score: {score}";
+            gameEndText.text = $"You lost";
+    }
+
+    private void EndWon() {
+        gameEndText.text = $"You won!";
+    }
+
+    private void runGame() {
+        scoreText.text = $"Score: {score}";
         if(Card.DisplayedCards.Count == 2 && !TempBlock) {
-            var chosen = Card.DisplayedCards;
-            Debug.Log($"{chosen[0].MatchingId} {chosen[1].MatchingId}");
-            if(chosen[0].MatchingId == chosen[1].MatchingId) {
+                var chosen = Card.DisplayedCards;
+                // Debug.Log($"{chosen[0].MatchingId} {chosen[1].MatchingId}");
+            if(chosen.Count() > 0 
+            && chosen.Where(x => x.MatchingId == chosen[0].MatchingId).Count() == chosen.Count()) {
                 TempBlock = true;
-                score += 10;
                 StartCoroutine(WaitToRemove(chosen.ToArray(), 0.5f));
             }
             else
             {
                 TempBlock = true;
-                score -= 2;
                 StartCoroutine(WaitToHide(chosen.ToArray(), 1f));
             }
         }
-        GameObject.Find("ScoreText").GetComponent<Text>().text = $"Score: {score}";
     }
 
     IEnumerator WaitToHide(Card[] chosen, float timeTillDisapear) {
+        score -= 2;
         yield return new WaitForSeconds(timeTillDisapear);
-        chosen[0].Hide();
-        chosen[1].Hide();
+        foreach(var item in chosen) {
+            item.Hide();
+        }
         Card.DisplayedCards = new List<Card>();
         TempBlock = false;
     }
     IEnumerator WaitToRemove(Card[] chosen, float timeTillDisapear) {
+        score += 10;
         yield return new WaitForSeconds(timeTillDisapear);
-        chosen[0].Remove();
-        chosen[1].Remove();
+        foreach(var item in chosen) {
+            item.Remove();
+        }
         Card.DisplayedCards = new List<Card>();
         TempBlock = false;
     }
