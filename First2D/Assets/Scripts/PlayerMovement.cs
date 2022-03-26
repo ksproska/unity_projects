13644061d53fa13375ae7082c2acc252c4e939a2
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-03-26 12:33:07 +0100

    very basic, but working player script

--- First2D/Assets/Scripts/PlayerMovement.cs
+++ First2D/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,22 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float speed = 2f;
+    private Rigidbody2D body;
+ 
+    private void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+ 
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.RightArrow))
+            body.velocity = new Vector2(speed, body.velocity.y);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            body.velocity = new Vector2(-speed, body.velocity.y);
     }
 }
