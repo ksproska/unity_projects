d109907352af88ed382a3b879dfc6d9254aa496a
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-03-27 15:28:15 +0200

    animation, 5 types

--- First2D/Assets/Scripts/PlayerMovement.cs
+++ First2D/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 // using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 2f;
     private Rigidbody2D body;
     private Rigidbody2D throwable;
+    public Animator animator;
 
     // private void OnEnable() {
     //     gameplayActions.Enable();
@@ -20,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
     {
         body = GetComponent<Rigidbody2D>();
         throwable = Resources.Load("Throwable", typeof(Rigidbody2D)) as Rigidbody2D;
+        body.freezeRotation = true;
     }
  
     private void Update()
@@ -32,6 +35,15 @@ public class PlayerMovement : MonoBehaviour
             body.velocity = new Vector2(-speed, body.velocity.y);
         if (Input.GetButtonDown("Fire1")) { //left ctrl
             var throwed = Instantiate(throwable) as Rigidbody2D;
+            animator.SetBool("Throws", true);
+            StartCoroutine(changeAnimationNormal(0.5f));
         }
+        animator.SetFloat("SpeedUp", body.velocity.y);
+        animator.SetFloat("SpeedBack", Mathf.Abs(body.velocity.x));
+    }
+
+    IEnumerator changeAnimationNormal(float sec) {
+        yield return new WaitForSeconds(sec);
+        animator.SetBool("Throws", false);
     }
 }
