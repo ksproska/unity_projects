bd17d18bf03a828497e353fa73f18e191fa4a7f1
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-05-07 09:47:03 +0200

    first person movement, floating points (no collection)

---
+++ First3D/Assets/MouseLook.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // https://www.youtube.com/watch?v=_QajrabyTJc&ab_channel=Brackeys
+    private float mouseSensivity = 900f;
+    public Transform playerBody;
+    float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
